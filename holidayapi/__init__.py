bf75891a5af654d9e893b6bdd36967c638514c0e
Daniel Bartolome <d.bartolome.r@gmail.com>
2018-07-18 14:58:35 -0500

    made it work

--- holidayapi/__init__.py
+++ holidayapi/__init__.py
@@ -1,24 +1,20 @@
+import os
 import json
 import requests
 
-class v1:
-    key = None
 
+class Api:
     def __init__(self, key):
-        self.key = key
+        self.key = os.environ['HOLIDAY_API_KEY']
 
-    def holidays(self, parameters):
+    def holidays(self, params):
         url = 'https://holidayapi.com/v1/holidays?'
 
-        if parameters.has_key('key') is False:
-            parameters['key'] = self.key
+        response = requests.get(url, params=params);
+        data = json.loads(response.text)
 
-        response = requests.get(url, params=parameters);
-        data     = json.loads(response.text)
-
-        if response.status_code != 200:
-            if data.has_key('error') is False:
-                data['error'] = 'Unknown error.'
+        if ('error' not in data.keys()) and (response.status_code != 200):
+            data['error'] = 'Unknown error'
 
         return data
 
